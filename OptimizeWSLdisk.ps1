65b81bbbef1a2b1a2b959dec1ccacd6052d2f85a
Alexis Lanquetin <alexis.lanquetin@gmail.com>
2024-11-14 10:37:41 +0100

    Add script V1

---
+++ OptimizeWSLdisk.ps1
@@ -0,0 +1,30 @@
+﻿# Shutdown WSL and kill WSL service
+wsl --shutdown
+Stop-Service -Name "LxssManager" -Force -ErrorAction SilentlyContinue
+Stop-Service -Name "wslservice" -Force -ErrorAction SilentlyContinue
+
+# List WSL distributions
+$wslDistros = wsl --list --quiet
+
+foreach ($distro in $wslDistros) {
+    # Retrieve the location of the ext4.vhdx file
+    $vhdxPath = (Get-ChildItem -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Lxss | Where-Object { $_.GetValue("DistributionName") -eq $distro }).GetValue("BasePath") + "\ext4.vhdx"
+
+    if ($vhdxPath) {
+        # Create a diskpart script to compact the vdisk
+        $diskpartScript = @"
+select vdisk file="$vhdxPath"
+attach vdisk readonly
+compact vdisk
+detach vdisk
+exit
+"@
+
+        # Execute the diskpart script
+        $diskpartScript | diskpart
+    }
+}
+
+# Restart WSL service
+Start-Service -Name "LxssManager" -ErrorAction SilentlyContinue
+Start-Service -Name "wslservice" -ErrorAction SilentlyContinue
